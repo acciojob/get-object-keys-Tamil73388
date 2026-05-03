d21740941b0e5c1b3c18e918fda4c6d27af1fca1
tamil <93432998+Tamil73388@users.noreply.github.com>
2026-05-03 16:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// The object provided in the problem
 let student = {
   name: "John Doe"
 };
 
 function getKeys(obj) {
 	ans=[]
-	for(let x of obj){
+	for(let x in obj){
 		ans.push(x)
 	}
 		return ans
 	
 }
 
-// The platform will likely test it like this:
 console.log(getKeys(student));
