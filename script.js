fd85ab5432de0eae9c25a4afa59498d40b26b609
tamil <93432998+Tamil73388@users.noreply.github.com>
2026-05-03 16:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 //your JS code here. If required.
+
 // Create the student object
 const student = {
   name: "tamil",
-	age:26,
-	city:"chennai"
+  age: 26,
+  city: "chennai"
 };
 
 // Function to get all keys from the object
 function getKeys(obj) {
-  let ans=[]
-	for(let x of obj){
-		ans.push(x)
-	}
-	return ans
+  return Object.keys(obj);
 }
 
 // Testing the function
-console.log(getKeys(student)); // Output: ["name"]
+console.log(getKeys(student));
