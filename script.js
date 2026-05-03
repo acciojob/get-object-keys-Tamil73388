f0625e3295aa12e774f5ddb81ff9d9cf877c89c2
tamil <93432998+Tamil73388@users.noreply.github.com>
2026-05-03 16:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // The object provided in the problem
-const student = {
+let student = {
   name: "John Doe"
 };
 
-// You need to add the getKeys method to the Object prototype
-Object.prototype.getKeys = function() {
-  // 'this' refers to the object calling the method (e.g., student)
-  return Object.keys(this);
-};
+function getKeys(obj) {
+	ans=[]
+	for(let x of obj){
+		ans.push(x)
+	}
+		return ans
+	
+}
 
 // The platform will likely test it like this:
-// console.log(student.getKeys());
+console.log(getKeys(student));
